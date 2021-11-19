c3565b48b8cc1948c74cc82f34188477c06273bb
dsincariuc <dmitri.sincariuc@post.cz>
2021-11-19 10:06:18 +0100

    Style ChatWindow component

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -3,45 +3,49 @@ import dayjs from "dayjs";
 
 import { Message } from "../utils/types";
 
+import classnames from "./ChatWindow.module.css";
+
 export interface Props {
-  messages: Message[];
-  messageInput: string;
-  handleSubmitForm: (event: React.FormEvent<HTMLFormElement>) => void;
-  handleChangeInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	messages: Message[];
+	messageInput: string;
+	handleSubmitForm: (event: React.FormEvent<HTMLFormElement>) => void;
+	handleChangeInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const ChatWindow: React.FC<Props> = ({
-  messages,
-  messageInput,
-  handleSubmitForm,
-  handleChangeInput,
+	messages,
+	messageInput,
+	handleSubmitForm,
+	handleChangeInput,
 }) => {
-  return (
-    <div>
-      <div className="chat">
-        <div>
-          {messages.map((message, i) => (
-            <div key={`${message.author}-${message.date}-${i}`}>
-              <div>
-                <span>{message.author}</span>{" "}
-                <span>{dayjs(message.date).format("h:mm A")}</span>
-              </div>
-              <p>{message.content}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <form onSubmit={handleSubmitForm}>
-            <input
-              type="text"
-              name="messageInput"
-              value={messageInput}
-              onChange={handleChangeInput}
-            />
-            <button>Send</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classnames.chatWrapper}>
+			<div className={classnames.chat}>
+				<div className={classnames.msgArea}>
+					{messages.map((message, i) => (
+						<div key={`${message.author}-${message.date}-${i}`}>
+							<div>
+								<span className={classnames.msgSender}>{message.author}</span>{" "}
+								<span className={classnames.msgDate}>{dayjs(message.date).format("h:mm A")}</span>
+							</div>
+							<p>{message.content}</p>
+						</div>
+					))}
+				</div>
+				<div>
+					<form onSubmit={handleSubmitForm}>
+						<input
+							className={classnames.msgInput}
+							type="text"
+							name="messageInput"
+							value={messageInput}
+							onChange={handleChangeInput}
+              placeholder="Share your thoughts..."
+						/>
+						<button className={classnames.button}>Send</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
