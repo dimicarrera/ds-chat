4c7aeb7aaecc43c7306e7df838dc5f3af6f9b6f7
dsincariuc <dmitri.sincariuc@post.cz>
2021-11-19 08:54:09 +0100

    Style Login component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,36 +1,41 @@
 import React from "react";
 
+import classnames from "./Input.module.css";
+
 export interface Props {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
-  error?: string;
-  value: string;
-  label: string;
-  placeholder: string;
-  name: string;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+	error?: string;
+	value: string;
+	label: string;
+	placeholder: string;
+	name: string;
 }
 
 export const Input: React.FC<Props> = ({
-  onChange,
-  onBlur,
-  error,
-  value,
-  label,
-  placeholder,
-  name,
+	onChange,
+	onBlur,
+	error,
+	value,
+	label,
+	placeholder,
+	name,
 }) => {
-  return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input
-        type="text"
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
-      {error && <div>{error}</div>}
-    </div>
-  );
+	return (
+		<div className={classnames.inputWrapper}>
+			<label htmlFor={name}>
+				{label}
+			</label>
+			<input
+				type="text"
+				name={name}
+				value={value}
+				placeholder={placeholder}
+				onChange={onChange}
+				onBlur={onBlur}
+				className={classnames.input}
+			/>
+			{error && <div className={classnames.inputError}>{error}</div>}
+		</div>
+	);
 };
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -12,102 +12,107 @@ import logo from "../assets/logo.png";
 import classnames from "./Login.module.css";
 
 export interface FormFields {
-  email: string;
-  username: string;
+	email: string;
+	username: string;
 }
 
 export interface FormErrors {
-  email?: string;
-  username?: string;
+	email?: string;
+	username?: string;
 }
 
 export const Login: React.FC = () => {
-  const [user, setUser] = useState<FormFields>({ email: "", username: "" });
-  const [errors, setErrors] = useState<FormErrors>({});
-  const dispatch = useDispatch();
+	const [user, setUser] = useState<FormFields>({ email: "", username: "" });
+	const [errors, setErrors] = useState<FormErrors>({});
+	const dispatch = useDispatch();
 
-  const validate = (name: string, value: string) => {
-    switch (name) {
-      case "email":
-        setErrors({ ...errors, [name]: validateEmail(value) });
-        break;
-      case "username":
-        setErrors({ ...errors, [name]: validateTextField(value) });
-        break;
-    }
-  };
+	const validate = (name: string, value: string) => {
+		switch (name) {
+			case "email":
+				setErrors({ ...errors, [name]: validateEmail(value) });
+				break;
+			case "username":
+				setErrors({ ...errors, [name]: validateTextField(value) });
+				break;
+		}
+	};
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    dispatch(login(user));
-    localStorage.setItem("user", JSON.stringify(user));
-  };
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		dispatch(login(user));
+		localStorage.setItem("user", JSON.stringify(user));
+	};
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
 
-    setUser({
-      ...user,
-      [name]: value,
-    });
+		setUser({
+			...user,
+			[name]: value,
+		});
 
-    if (errors[name as keyof FormErrors]) {
-      validate(name, value);
-    }
-  };
+		if (errors[name as keyof FormErrors]) {
+			validate(name, value);
+		}
+	};
 
-  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    validate(name, value);
-  };
+	const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+		validate(name, value);
+	};
 
-  const isLoginButtonDisabled = () => {
-    if (!user.email || !user.username) {
-      return true;
-    }
-    return false;
-  };
+	const isLoginButtonDisabled = () => {
+		if (!user.email || !user.username) {
+			return true;
+		}
+		return false;
+	};
 
-  return (
-    <div className={classnames.loginWrapper}>
-      <div className={classnames.container}>
-        <div className={classnames.loginHeading}>
-          <img
-            src={logo}
-            alt="DS chat demo app"
-            className={classnames.loginLogo}
-          />
-          <div className={classnames.loginGreeting}>
-            <h1 className={classnames.loginH1}>
-              Welcome{user.username && ","}
-            </h1>
-            <h2 className={classnames.loginH2}>{user.username}</h2>
-          </div>
-        </div>
-        <form onSubmit={handleSubmit} className={classnames.form}>
-          <Input
-            label="Email Address"
-            name="email"
-            value={user.email}
-            placeholder="any@email.com"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.email}
-          />
-          <Input
-            label="Username"
-            name="username"
-            value={user.username}
-            placeholder="very_cool_username"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.username}
-          />
-          <div>
-            <button disabled={isLoginButtonDisabled()}>Sign in</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classnames.loginWrapper}>
+			<div className={classnames.container}>
+				<div className={classnames.loginHeading}>
+					<img
+						src={logo}
+						alt="DS chat demo app"
+						className={classnames.loginLogo}
+					/>
+					<div className={classnames.loginGreeting}>
+						<h1 className={classnames.loginH1}>
+							Welcome{user.username && ","}
+						</h1>
+						<h2 className={classnames.loginH2}>{user.username}</h2>
+					</div>
+				</div>
+				<form onSubmit={handleSubmit} className={classnames.form}>
+					<Input
+						label="Email Address"
+						name="email"
+						value={user.email}
+						placeholder="any@email.com"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.email}
+					/>
+					<Input
+						label="Username"
+						name="username"
+						value={user.username}
+						placeholder="very_cool_username"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.username}
+					/>
+					<div>
+						<button
+							className={classnames.button}
+							disabled={isLoginButtonDisabled()}
+						>
+							Sign In
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
