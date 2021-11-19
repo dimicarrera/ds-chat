3089579a60e1d6456c45c8755faa81b18cd2fe72
dsincariuc <dmitri.sincariuc@post.cz>
2021-11-19 09:09:07 +0100

    Style Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,23 @@
 import logo from "../assets/logo.png";
 
+import classnames from "./Navbar.module.css";
+
 export interface Props {
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+	onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export const Navbar: React.FC<Props> = ({ onClick }) => {
-  return (
-    <div className="nav">
-      <div>
-        <img src={logo} alt="ds demo chat logo" />
-        <h1>DS Demo Chat App</h1>
-      </div>
-      <button type="button" onClick={onClick}>
-        Quit
-      </button>
-    </div>
-  );
+	return (
+		<div className={classnames.nav}>
+			<div className={classnames.heading}>
+				<img src={logo} alt="ds demo chat logo" className={classnames.logo} />
+				<h1>Dmitri's Comfy Chat</h1>
+			</div>
+			<div>
+				<button type="button" onClick={onClick} className={classnames.button}>
+					Sign Out
+				</button>
+			</div>
+		</div>
+	);
 };
